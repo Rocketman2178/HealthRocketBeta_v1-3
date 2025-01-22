413c1f7d08ce888cd785b6d3a336efae26ce4221
GetAs0938 <getnetasrat0938@gmail.com>
2025-01-22 21:50:50 +0300

    remove unused import

--- src/components/dashboard/challenge/ChallengePage.tsx
+++ src/components/dashboard/challenge/ChallengePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { ChallengeDetails } from './ChallengeDetails';
 import { challenges, tier0Challenge } from '../../../data/challenges';
