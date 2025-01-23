3b0a4076114174e6c636bb82262def05125ca884
GetAs0938 <getnetasrat0938@gmail.com>
2025-01-23 15:33:01 +0300

    update migration

--- supabase/migrations/20250107123041_broken_shadow.sql
+++ supabase/migrations/20250107123041_broken_shadow.sql
@@ -131,7 +131,7 @@ BEGIN
 
     -- Update user's burn streak
     UPDATE users
-    SET burn_streak = burn_streak + 0
+    SET burn_streak = burn_streak
     WHERE id = p_user_id;
 
     RETURN jsonb_build_object(
